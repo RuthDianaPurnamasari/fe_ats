974767668ca75a8b8e63ea2a0c330adc7ead6a35
RuthDianaPurnamasari <ruthdianaps@gmail.com>
2024-05-11 01:55:47 +0700

    baru

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,11 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js";
 let urlAPI = "https://ws-atsruth2024-e7b20d58134d.herokuapp.com/penelitian";
-get(urlAPI,isiTablePresensi);
-function isiTablePresensi(results){
+get(urlAPI,isiTablePenelitian);
+function isiTablePenelitian(results){
     console.log(results);
+    results.forEach(isiRow);
+}
+function isiRow(value){
+    console.log(value)
 }
